ad4f77df33b1ea9d5922bf1bc54fa662be05b17c
sankar303065 <120238889+sankar303065@users.noreply.github.com>
2023-09-26 15:04:03 +0530

    Add files via upload

---
+++ Day 8/src/components/pages/termsandconditions.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import '../../assets/css/termsandconditions.css';
+
+function TermsAndConditions() {
+    return (
+        <div className="termback">
+      <div className="container">
+        <header>
+          <h1 className="termback">Terms and Conditions</h1>
+        </header>
+        <div className="termcontent">
+          <section>
+            <h2>1. Introduction</h2>
+            <p>These terms and conditions ("Agreement") govern the provision of digital marketing services ("Services") by [Your Company Name] ("Company," "we," "us," or "our") to the client ("Client" or "you"). By using our Services, you agree to abide by and be bound by this Agreement.</p>
+          </section>
+  
+          <section>
+            <h2>2. Services</h2>
+            <p>2.1. <strong>Scope:</strong> We will provide digital marketing services as described in the agreed-upon proposal or contract.</p>
+            <p>2.2. <strong>Modification:</strong> Any changes or modifications to the Services must be agreed upon in writing by both parties.</p>
+          </section>
+  
+          <section>
+            <h2>3. Client Responsibilities</h2>
+            <p>3.1. <strong>Cooperation:</strong> Client agrees to cooperate with us by providing necessary information, access to accounts, and timely feedback to facilitate the provision of Services.</p>
+            <p>3.2. <strong>Content:</strong> Client is responsible for providing any content, materials, or assets required for the Services unless otherwise specified in the agreement.</p>
+          </section>
+  
+          <section>
+            <h2>4. Fees and Payment</h2>
+            <p>4.1. <strong>Fees:</strong> Client agrees to pay the fees as outlined in the proposal or contract.</p>
+            <p>4.2. <strong>Payment Terms:</strong> Payments are due as specified in the agreement. Late payments may result in suspension of Services.</p>
+          </section>
+  
+          <section>
+            <h2>5. Term and Termination</h2>
+            <p>5.1. <strong>Term:</strong> The term of this Agreement will commence on the effective date and continue until terminated by either party as specified in the agreement.</p>
+            <p>5.2. <strong>Termination:</strong> Either party may terminate this Agreement with written notice for a material breach or as otherwise specified in the agreement.</p>
+          </section>
+  
+          <section>
+            <h2>6. Confidentiality</h2>
+            <p>Both parties agree to maintain the confidentiality of any proprietary or confidential information disclosed during the provision of Services.</p>
+          </section>
+  
+          <section>
+            <h2>7. Intellectual Property</h2>
+            <p>7.1. <strong>Ownership:</strong> Unless otherwise specified, all intellectual property rights related to the Services and any deliverables shall remain the property of the Company.</p>
+            <p>7.2. <strong>License:</strong> The Company may grant the Client a non-exclusive license to use any deliverables for the purposes outlined in the agreement.</p>
+          </section>
+  
+          <section>
+            <h2>8. Limitation of Liability</h2>
+            <p>To the extent permitted by law, our liability for any damages or losses arising from the provision of Services is limited to the fees paid by the Client.</p>
+          </section>
+  
+          <section>
+            <h2>9. Indemnification</h2>
+            <p>Client agrees to indemnify and hold harmless the Company from any claims, losses, or liabilities arising from the Client's use of the Services.</p>
+          </section>
+  
+          <section>
+            <h2>10. Governing Law and Jurisdiction</h2>
+            <p>This Agreement is governed by the laws of [Your Jurisdiction]. Any disputes will be subject to the exclusive jurisdiction of the courts in [Your Jurisdiction].</p>
+          </section>
+  
+          <section>
+            <h2>11. Miscellaneous</h2>
+            <p>11.1. <strong>Entire Agreement:</strong> This Agreement constitutes the entire understanding between the parties and supersedes all prior agreements or understandings.</p>
+            <p>11.2. <strong>Amendment:</strong> Any amendments to this Agreement must be in writing and signed by both parties.</p>
+          </section>
+        <footer>
+          <p>&copy; 2023 Your Company</p>
+        </footer>
+      </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default TermsAndConditions;
